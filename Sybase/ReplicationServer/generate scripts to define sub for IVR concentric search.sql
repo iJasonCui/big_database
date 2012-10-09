9aa121ab9e9faa0134648c752a6764451f69f112
iJasonCui <jcui@jcui-wrkstn.fmginc.com>
2012-10-09 15:20:52 -0400

    sybase rep server

--- Sybase/ReplicationServer/generate scripts to define sub for IVR concentric search.sql
+++ Sybase/ReplicationServer/generate scripts to define sub for IVR concentric search.sql
@@ -17,10 +17,10 @@ select r.* , s.subid, s.subname
 from rs_objects r, rs_subscriptions s 
 where r.phys_tablename = "Mailbox" and r.objid = s.objid and r.ownertype = 'U' and s.subname like "%_searchDB_%"
 
-
---select 'drop replication definition "' + objname  + '"' from tempdb..RepNoSub where subid is null
 --define subscription "5queLLF_Session_searchDBFr" for "13queLLF_Session"  with replicate at v151dbp05ivr.searchDBFrench
 
---select 'define subscription "5' + substring(objname,3,20)  + '_searchDB" for ' + '"' + objname + '"' 
---       + '   with replicate at v151dbp05ivr.searchDB'
---from tempdb..RepNoSub where subid is not null
+select 'define subscription "5' + substring(r.objname,3,20)  + '_searchDB" for ' + '"' + r.objname + '"' 
+       + '   with replicate at v151dbp05ivr.searchDB'
+from rs_objects r, rs_subscriptions s 
+where r.phys_tablename = "Mailbox" and r.objid = s.objid and r.ownertype = 'U' and s.subname like "%_searchDB_%"
+
