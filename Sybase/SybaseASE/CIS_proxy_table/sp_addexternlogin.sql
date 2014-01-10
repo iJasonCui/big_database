b91c01b46756f617e45d4ecffeeae881daa1d6bf
Jason Cui <jasoncui500@gmail.com>
2014-01-10 11:45:23 -0500

    Proxy table CIS

--- Sybase/SybaseASE/CIS_proxy_table/sp_addexternlogin.sql
+++ Sybase/SybaseASE/CIS_proxy_table/sp_addexternlogin.sql
@@ -6,33 +6,33 @@ sp_helpexternlogin
 
 select name, * from syslogins
 
-exec sp_addexternlogin  g151dbr07,	iwong,	webmaint, free2stay
+exec sp_addexternlogin  g151dbr07,	iwong,	webmaint, [Password]
 --sp_addexternlogin  g151dbr07,	aalb,	webmaint
-exec sp_addexternlogin  g151dbr07,	amcnam,	webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	aradu,	webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	bi	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	bvo	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	ccd_cron	,webmaint, free2stay
---exec sp_addexternlogin  g151dbr07,	cis_user	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	cron_ccd	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	cron_dm	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	cron_sa	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	cube	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	dmteam	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	finance	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	finance900	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	financeSun	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	hqian	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	ibang	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	jcui	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	read_only_user	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	remotefrom0r	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	reports	,webmaint, free2stay
+exec sp_addexternlogin  g151dbr07,	amcnam,	webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	aradu,	webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	bi	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	bvo	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	ccd_cron	,webmaint, [Password]
+--exec sp_addexternlogin  g151dbr07,	cis_user	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	cron_ccd	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	cron_dm	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	cron_sa	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	cube	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	dmteam	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	finance	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	finance900	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	financeSun	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	hqian	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	ibang	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	jcui	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	read_only_user	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	remotefrom0r	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	reports	,webmaint, [Password]
 --exec sp_dropexternlogin  g151dbr07,	syi	
---exec sp_addexternlogin  g151dbr07,	syi	,webmaint, free2stay, free2stay
-exec sp_addexternlogin  g151dbr07,	testuser	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	tkchan	,webmaint, free2stay
-exec sp_addexternlogin  g151dbr07,	web	,webmaint, free2stay
+--exec sp_addexternlogin  g151dbr07,	syi	,webmaint, [Password], [Password]
+exec sp_addexternlogin  g151dbr07,	testuser	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	tkchan	,webmaint, [Password]
+exec sp_addexternlogin  g151dbr07,	web	,webmaint, [Password]
 
 
 create database Accounting1g4100 on [db_device_name] = [size]
